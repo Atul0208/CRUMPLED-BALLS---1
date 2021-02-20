5f34b41d9fb83c96c63ab0b48ae3f421655b1d7c
Atul0208 <78468687+Atul0208@users.noreply.github.com>
2021-02-20 20:13:28 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,51 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var engine,world;
+
+
+function setup() {
+	createCanvas( 3000,1200);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+downDustbin=new D(750+1800,960+200+50-39,279,26)
+lDustbin=new D(600+1800,847+200+50-39+2,26,250)
+rDustbin=new D(899+1800,847+200+50-39+2-127,26,250+257)
+ed=new D(2685,700-30,70,20)
+g=new Ground(1500,1192,3000,15)
+paper=new Paper(200,1188,34)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  
+  Engine.update(engine)
+  background(0);
+  lDustbin.display();
+  g.display();
+  downDustbin.display();
+  lDustbin.display();
+  rDustbin.display();
+  ed.display();
+  paper.display();
+  keyPressed();
+  drawSprites();
+ 
+}
+function keyPressed(){
+  if(keyCode===UP_ARROW){
+Matter.Body.applyForce(paper.body,paper.body.position,{x:33,y:-16})
+  }
+}
+
+
+
